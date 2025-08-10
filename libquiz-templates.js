aa672ea8fbe2707da7745f5b456e61c8836c151c
OrbitaCompany <orbitacompanys@gmail.com>
2025-08-10 15:33:41 -0300

    Create libquiz-templates.js

---
+++ libquiz-templates.js
@@ -0,0 +1,38 @@
+// Modelos de quiz criptografados
+export const quizzes = {
+  "cliente-123": {
+    title: "Quiz de Filmes",
+    questions: [
+      {
+        question: "Qual filme ganhou o Oscar de Melhor Filme em 2023?",
+        options: [
+          "Tudo em Todo Lugar ao Mesmo Tempo",
+          "Avatar: O Caminho da Água",
+          "Top Gun: Maverick",
+          "Os Banshees de Inisherin"
+        ],
+        answer: 0
+      },
+      {
+        question: "Qual ator interpretou Batman em 'The Batman' (2022)?",
+        options: [
+          "Ben Affleck",
+          "Robert Pattinson",
+          "Christian Bale",
+          "Michael Keaton"
+        ],
+        answer: 1
+      }
+    ]
+  },
+  "cliente-abc": {
+    title: "Quiz de Música",
+    questions: [
+      {
+        question: "Quem é conhecido como 'Rei do Pop'?",
+        options: ["Michael Jackson", "Elvis Presley", "Freddie Mercury", "Prince"],
+        answer: 0
+      }
+    ]
+  }
+};
